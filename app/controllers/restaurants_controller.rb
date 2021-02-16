9a969a87f4abba21bc89e3927e6e631b98944b26
Frederic Desmarais <fdesmarais.them@gmail.com>
2021-02-15 20:50:57 -0500

    chefs name added to restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -45,6 +45,11 @@ class RestaurantsController < ApplicationController
     redirect_to restaurants_url, notice: 'Restaurant was successfully destroyed.'
   end
 
+
+  def top
+    @restaurants = Restaurant.where(stars: 5)
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_restaurant
